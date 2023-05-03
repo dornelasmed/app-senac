08f87dbb97d16ffd68499bc3a1d88f318af1918b
dornelasmed <131292994+dornelasmed@users.noreply.github.com>
2023-05-03 17:32:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert(" Este aplicativo foi criado pr João Donizeth Dornelas")
+//alert (" este aplicativo foi desenvolvido por Joao Donizeth Dornelas")
+
+
+var nome = prompt("Qual o seu nome?")
+var sobrenome = prompt("Qual o é seu sobrenome?")
+
+
+alert("seja bem vindo, " + nome + " "+ sobrenome)
