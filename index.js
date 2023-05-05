e26a88c865d33a6dad130613bc1cf94fde81ce63
dornelasmed <131292994+dornelasmed@users.noreply.github.com>
2023-05-05 17:09:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,59 @@
 //alert (" este aplicativo foi desenvolvido por Joao Donizeth Dornelas")
 
 
-var nome = prompt("Qual o seu nome?")
-var sobrenome = prompt("Qual o é seu sobrenome?")
+//var nome = prompt("Qual o seu nome?")
+//var sobrenome = prompt("Qual o é seu sobrenome?")
 
 
-alert("seja bem vindo, " + nome + " "+ sobrenome)
+//alert("seja bem vindo, " + nome + " " + sobrenome)
+
+
+// criar uma lista com os alunos presentes
+
+//console.log(alunosPresentes)
+
+//var numeros =[10, 45, 80, 100];
+
+//console.log(numeros[0] * numeros[2]);
+/*var pessoa = {
+
+nome: "Joao"
+idade: 25, 
+estadoCivil: "em relacionamento" 
+}
+*/
+
+//console.log(pessoa.nome + " tem " + pessoa.idade + " anos e esta " + pessoa.estadoCivil)
+/*
+var curso = {
+    nome: "Programacao",
+    cargaHoraria: 200,
+    alunosPresentes: ["João", "Maria", "Diego", "Guilherme", "Gabriela", "Felipe", "Alvaro", "Matheus", "Danilo", "Luiz Felipe", "Marcelo", "Izabele", "Júlia", "Marcel", "Kaua", "Cauã", "Nawany", "Everton",],
+}
+
+//console.log("o curso de " + curso.nome+ " tem "+ curso.cargaHoraria+" horas e os alunos frequentes são " + curso.alunosPresentes+".")
+
+//var filme = {
+   // nome: "0 Predador",
+    //ator: "Arnold Schwarzenegger",
+   // Diretor: "John Mc Tirnan",
+   // Lancamento: 1990,
+{
+
+
+//console.log("filme de" + filme.nome + " com o ator " + filme.ator + "Do Diretor" + filme.Diretor + " e o lancamento foi "+ filme.Lancamento)
+*/
+ 
+var senha = "senha123";
+
+if (senha == "senha123"){
+    console.log("LOGANDO NO SISTEMA")
+}
+else {
+    console.log("SENHA INCORRETA")
+}
+
+
+    
+
+
